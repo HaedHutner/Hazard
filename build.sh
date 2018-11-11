f966605b1c3aa0a3dbb8e36c583e68c8316256e6
haedhutner <mvvasilev@outlook.com>
2018-11-11 16:56:40 +0200

    Improve build script

--- build.sh
+++ build.sh
@@ -1,18 +1,13 @@
 #!/bin/bash
 
+if [ ! -d "build" ]; then
+    mkdir build
+fi
+
+cd build
+
 if [ "$1" != "" ] && [ "$1" == "clean" ]; then
-    if [ ! -d "build" ]; then
-        mkdir build
-    fi
-
-    cd build
     rm -rf *
-else
-    if [ ! -d "build" ]; then
-        mkdir build
-    fi
-
-    cd build
 fi
 
 cmake ..
